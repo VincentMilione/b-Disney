02dc320ff8835fba558b2ea0dce00646f808bcc8
Claudiabuono <52284939+Claudiabuono@users.noreply.github.com>
2019-07-04 22:52:18 +0200

    animate

--- WebContent/js/animation.js
+++ WebContent/js/animation.js
@@ -3,7 +3,7 @@
  ($(".productCard").hover(function(){
       $(this).animate({width:"+=1%", height:"+=1%", marginLeft:"-=1%", marginRight: "-=1%", marginTop:"-=1%", marginBottom:"-=1%"});
     }, function() {
-          $(this).animate({width:"-=1%",height:"-=1%", marginLeft:"+=1%", marginRight: "+=1%", marginTop:"+=1%", marginBottom:"+=1%"});
-  })
+       $(this).animate({width:"-=1%",height:"-=1%", marginLeft:"+=1%", marginRight: "+=1%", marginTop:"+=1%", marginBottom:"+=1%"});
+    })
   );
 
