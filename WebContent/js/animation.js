a800390b04c4e0d4b0b0ee1c45db01c7859f08f1
Renerine <52202415+Renerine@users.noreply.github.com>
2019-07-05 09:55:27 +0200

    Update animation.js

--- WebContent/js/animation.js
+++ WebContent/js/animation.js
@@ -1,9 +1,12 @@
 
 
- ($(".productCard").hover(function(){
-      $(this).animate({width:"+=1%", height:"+=1%", marginLeft:"-=1%", marginRight: "-=1%", marginTop:"-=1%", marginBottom:"-=1%"});
-    }, function() {
-       $(this).animate({width:"-=1%",height:"-=1%", marginLeft:"+=1%", marginRight: "+=1%", marginTop:"+=1%", marginBottom:"+=1%"});
-    })
-  );
-
+$(document).ready(function () {
+	
+	$(".productCard").hover(function(){
+	      $(this).animate({width:"+=1%", height:"+=1%", marginLeft:"-=1%", marginRight: "-=1%", marginTop:"-=1%", marginBottom:"-=1%"});
+		}, function() {
+	          $(this).animate({width:"-=1%",height:"-=1%", marginLeft:"+=1%", marginRight: "+=1%", marginTop:"+=1%", marginBottom:"+=1%"},{
+	        	    complete: function() { $(this).removeAttr('style') }
+	        	});
+	    })
+});
